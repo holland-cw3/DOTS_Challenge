53b00befb7615fd9c33675541a46f1f4775c460f
Caleb H <hollandcw3@gmail.com>
2025-03-01 20:48:24 -0500

    hi

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+
+import "./App.css";
+import map from './map.png'
+
+function App() {
+  return (
+    <div>
+
+      <div class=''>
+        
+      </div>
+      <TransformWrapper>
+        <TransformComponent>
+          <img src={map} alt="test" />
+        </TransformComponent>
+      </TransformWrapper>
+      </div>
+
+  );
+}
+
+export default App;
