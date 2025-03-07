96bbdf3d3151bf515fc4333164ec3ad34807df72
Caleb H <hollandcw3@gmail.com>
2025-03-07 00:20:52 -0500

    hi

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,118 @@
-import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
-
 import "./App.css";
-import map from './map.png'
+import logo from "./Polka.png";
+import { DataGrid } from "@mui/x-data-grid";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import { MdAttachMoney } from "react-icons/md";
+import { LuClipboardList } from "react-icons/lu";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
 
+import { useState, useEffect } from "react";
 
+const columns = [
+  { field: "permit", headerName: "Permit Name", width: 350 },
+];
 
+const rows = [
+  { id: 1, permit: "hi" },
+  { id: 2, permit: "" },
+  { id: 3, permit: "" },
+  { id: 4, permit: "" },
+  { id: 5, permit: "Targaryen" },
+  { id: 6, permit: "Melisandre" },
+  { id: 7, permit: "Clifford" },
+  { id: 8, permit: "Frances" },
+  { id: 9, permit: "Roxie" },
+];
 
+const paginationModel = { page: 0, pageSize: 5 };
 
+export default function App() {
+  const [date, setDate] = useState(null);
+  const [time, setTime] = useState(null);
+  const [perms, setPerms] = useState([]);
 
+  const handleSelectionChange = (selectionModel) => {
+    const selectedPerms = selectionModel.map((id) => rows.find((row) => row.id === id));
+    setPerms(selectedPerms);
+  };
 
-function App() {
   return (
-    <div>
-
-      <div class=''>
-        
+    <div className="split">
+      <div className="search">
+        <div className="subSearch">
+          <div className="center">
+            <img src={logo} alt="logo" className="logo" />
+          </div>
+          <hr />
+          <form>
+            <div className="center">
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  label="Date"
+                  value={date}
+                  onChange={(newDate) => setDate(newDate)}
+                  sx={{
+                    backgroundColor: "white",
+                    borderRadius: "5px",
+                    width: "90%",
+                  }}
+                />
+              </LocalizationProvider>
+            </div>
+            <div className="center">
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <TimePicker
+                  label="Time"
+                  value={time}
+                  onChange={(newTime) => setTime(newTime)}
+                  sx={{
+                    backgroundColor: "white",
+                    borderRadius: "5px",
+                    width: "90%",
+                  }}
+                />
+              </LocalizationProvider>
+            </div>
+            <div className="center">
+              <Paper sx={{ height: "50vh", width: "90%" }}>
+                <DataGrid
+                  rows={rows}
+                  columns={columns}
+                  initialState={{ pagination: { paginationModel } }}
+                  pageSizeOptions={[5, 10]}
+                  checkboxSelection
+                  onRowSelectionModelChange={handleSelectionChange}
+                  sx={{ border: 0 }}
+                />
+              </Paper>
+            </div>
+            <div className="center">
+              <Button
+                variant="contained"
+                sx={{
+                  backgroundColor: "red",
+                  marginRight: "1vw",
+                  width: "43%",
+                }}
+                startIcon={<LuClipboardList />}
+              >
+                Parking Rules
+              </Button>
+              <Button
+                variant="contained"
+                sx={{ backgroundColor: "red", width: "43%" }}
+                startIcon={<MdAttachMoney />}
+              >
+                Parking Fines
+              </Button>
+            </div>
+          </form>
+        </div>
       </div>
-      <TransformWrapper>
-        <TransformComponent>
-          <img src={map} alt="test" />
-        </TransformComponent>
-      </TransformWrapper>
-      </div>
-
+    </div>
   );
 }
-
-export default App;
